9e95c825fc6bc932a71bcc4252fa01a84721d228
Anuradha Kumari <anuradha.kumari@CaptainMarvel.local>
2019-09-02 01:26:25 +0530

    creating simple add notes app with hooks

---
+++ src/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className="app-header">
+      <h2>Let's create notes</h2>
+    </div>
+  )
+};
+
+export default Header;
