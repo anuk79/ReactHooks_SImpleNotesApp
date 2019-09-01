8de10c242a2ef3a1cea939eb45ec4df09e0c3042
Anuradha Kumari <anuradha.kumari@CaptainMarvel.local>
2019-09-02 01:46:54 +0530

    Updates

---
+++ precache-manifest.3198ce3f4ea5379c5c6b0ac7c42f761c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c661fbd3f842f5342f4a3fce36fb3720",
+    "url": "/ReactHooks_SImpleNotesApp/index.html"
+  },
+  {
+    "revision": "f208dc8626abffc403e2",
+    "url": "/ReactHooks_SImpleNotesApp/static/css/main.2cb2917f.chunk.css"
+  },
+  {
+    "revision": "226e0d445a2faf69769a",
+    "url": "/ReactHooks_SImpleNotesApp/static/js/2.d2922a89.chunk.js"
+  },
+  {
+    "revision": "f208dc8626abffc403e2",
+    "url": "/ReactHooks_SImpleNotesApp/static/js/main.be11ae03.chunk.js"
+  },
+  {
+    "revision": "a2a4e004e67117cc3f8c",
+    "url": "/ReactHooks_SImpleNotesApp/static/js/runtime~main.aa344257.js"
+  }
+]);
